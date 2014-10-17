c05c3dd7ea2348128eeaf0c3fa2fbe2a7157ca5f
nafSadh <khan@nafsadh.com>
2014-10-16 21:32:53 -0400

    report maker

--- project2/multiagent/reporter.py
+++ project2/multiagent/reporter.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 start_time = datetime.now()
 print "start:", start_time.strftime("%Y-%m-%d %H:%M:%S")
-os.system("python pacman.py -p MinimaxAgent -l minimaxClassic -a depth=4 --numGames 1000 --frameTime 0 --fixRandomSeed --q")
+os.system("python pacman.py -p AlphaBetaAgent -l smallClassic -a depth=3 --numGames 20 --frameTime 0 --fixRandomSeed -q")
 end_time = datetime.now()
 print "done:", end_time.strftime("%Y-%m-%d %H:%M:%S")
 print "time taken:",(end_time-start_time), "seconds"
